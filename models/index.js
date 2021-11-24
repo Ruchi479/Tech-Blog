edb33208c4834b368f972eb6de295c8ad0c985f7
Ruchi <ruchisaini.0404@gmail.com>
2021-11-24 14:23:40 +1100

    Created associations and setup Models

---
+++ models/index.js
@@ -0,0 +1,37 @@
+//import all models
+const User= require('./User');
+const User= require('./Post');
+const User= require('./Comment');
+
+//create association
+//user can make many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//a post can only belong to one user
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+//Comment can only belong to one user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+//a particular comment can only belong to one post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+//user can make many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+//a post has many comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment};
